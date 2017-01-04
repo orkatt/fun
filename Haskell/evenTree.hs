4f8a8c58189e95e17058f45401366b18b9891701
orkatt <Alexander.E.Maklakov@gmail.com>
2017-01-04 15:12:33 +0400

    beat hackerrank cases

--- Haskell/evenTree.hs
+++ Haskell/evenTree.hs
@@ -3,9 +3,8 @@
 
 module EvenTree where
 
-
 data Tree n = Nil | Leaf n | Tree n [Tree n] | Fail
-    deriving (Show)
+      deriving (Show)
 
     
 link :: Tree String -> String -> String -> Tree String
@@ -23,17 +22,7 @@ link (Tree n cs) s t
         where f x = link x s t
 
 
-readN :: Tree String -> IO (Tree String)
-
-readN t = 
-  do l <- getLine
-     if l /= ""
-     then readN (join t (words l))
-     else return t
-  where join x (n1:n2:_) = link x n2 n1
-
-
-  
+ 
 --
 class Countable t where
    count :: t -> Int
@@ -48,14 +37,33 @@ instance Countable (Tree n) where
 removableEdges :: Tree String -> Int
 removableEdges Nil         = 0    
 removableEdges (Leaf _)    = 0
-removableEdges (Tree _ cs) = sum (map f cs)
+removableEdges (Tree _ cs) = sum (map f cs) + sum (map removableEdges cs)
                              where f x = if even(count(x)) then 1 else 0
+
+
+readN :: Int -> Tree String -> IO (Tree String)
+
+readN n t = 
+   if n /= 0 
+   then   
+      do l <- getLine
+         if l /= ""
+         then readN (n-1) (join t (words l))
+         else return t
+   else return t
+   where join x (n1:n2:_) = link x n2 n1
+
+
+
 
 
 --   
 main = 
-  do t <- readN Nil
-     putStrLn (show t)
-     
+  do fl <- getLine
+     let n = read (last (words fl)) :: Int
+     --putStrLn (show n)
+     t <- readN n Nil
+     --putStrLn (show t)
+     putStrLn (show (removableEdges(t)))     
 
 
